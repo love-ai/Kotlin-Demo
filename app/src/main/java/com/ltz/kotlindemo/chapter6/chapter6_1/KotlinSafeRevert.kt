78d1123a4928a2436edecc69a4221c5cfefb888a
zhuxiaowei <zhuxiaowei@zuinianqing.com>
2018-09-07 20:25:07 +0800

    finish chapter6_1_5

--- app/src/main/java/com/ltz/kotlindemo/chapter6/chapter6_1/KotlinSafeRevert.kt
+++ app/src/main/java/com/ltz/kotlindemo/chapter6/chapter6_1/KotlinSafeRevert.kt
@@ -8,15 +8,15 @@ fun print6_1_5() {
 
     val s1 = Student("xiao", "hong")
     val s2 = Student("xiao", "hong")
-    println(s1 == s2)
-    println(s1.equals(s2))
-
+    println(s1 == s2)//true ==运算符会调用equals方法
+    println(s1.equals(s2))//true
+    println(s1.equals(34))//true
 }
 
 class Student(val firstName: String, val lastName: String) {
     override fun equals(o: Any?): Boolean {
         val otherStudent = o as? Student ?: return false
-        return o.firstName == firstName  && o.lastName == lastName
+        return otherStudent.firstName == firstName  && otherStudent.lastName == lastName
     }
 
     override fun hashCode(): Int = firstName.hashCode() * 37 + lastName.hashCode()
